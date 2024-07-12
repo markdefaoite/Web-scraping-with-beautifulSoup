1d9ba066110c0ab508096dd54b02674cffc20ca1
markdefaoite <markdefaoite@hotmail.com>
2024-07-12 13:56:33 -0400

    Web-scraping-with-beautifulSoup - read the 100 greatest movies from Empireonline.com and output the movies to a txt file

--- top 100 movies.py
+++ top 100 movies.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 # read the 100 greatest movies from Empireonline.com and output the movies to a txt file
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
-
+response = requests.get(URL)
 empire_web_page = response.text
 soup = BeautifulSoup(empire_web_page, 'html.parser')
 
@@ -13,8 +13,6 @@ movie_titles = []
 
 for movie in movies:
     movie_titles.insert(0, movie.getText())
-
-print(movie_titles)
 
 with open("100 greatest movies.txt", mode="w") as file:
     for movie in movie_titles:
